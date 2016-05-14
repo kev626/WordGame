aab5d9f39060eecb380a0737ced27070cacfa3fe
Kevin Raneri <kevin.raneri@gmail.com>
2016-05-14 18:06:12 -0400

    Added callback registration to MentionListener

--- WordGame/src/wordgame/WordGame.java
+++ WordGame/src/wordgame/WordGame.java
@@ -6,9 +6,16 @@
 
 package wordgame;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Scanner;
+import twitter4j.FilterQuery;
 import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+import twitter4j.TwitterStream;
+import twitter4j.TwitterStreamFactory;
+import twitter4j.conf.ConfigurationBuilder;
 
 /**
  *
@@ -34,7 +41,21 @@ public class WordGame {
     }
     
     public static void initGame (String file) throws FileNotFoundException {
-        
+        Scanner s = new Scanner(new File(file));
+        ConfigurationBuilder cb = new ConfigurationBuilder();
+        cb.setDebugEnabled(true)
+                .setOAuthConsumerKey(s.nextLine())
+                .setOAuthConsumerSecret(s.nextLine())
+                .setOAuthAccessToken(s.nextLine())
+                .setOAuthAccessTokenSecret(s.nextLine());
+        TwitterFactory tf = new TwitterFactory(cb.build());
+        t = tf.getInstance();
+        TwitterStreamFactory twitterStreamFactory = new TwitterStreamFactory(t.getConfiguration());
+        TwitterStream twitterStream = twitterStreamFactory.getInstance();
+        FilterQuery filterQuery = new FilterQuery();
+        filterQuery.follow(new long[]{3939785352L});
+        twitterStream.addListener(new MentionListener());
+        twitterStream.filter(filterQuery);
     }
     
 }
